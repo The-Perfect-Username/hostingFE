a112a6679273829c2d534ecadc68ff0024c5cf2b
Lachlan Enderlin <lendo1995@gmail.com>
2017-04-04 20:49:25 +1000

    .

--- public/js/settings.js
+++ public/js/settings.js
@@ -4,4 +4,47 @@ $(document).ready(function() {
         e.preventDefault();
     });
 
+    $(document).on("click", "#swapEmails", function(e) {
+        e.preventDefault();
+        var primary = $("#primaryEmail");
+        var secondary = $("#secondaryEmail");
+        var primaryEmail = primary.val();
+        var secondaryEmail = secondary.val();
+
+        if ((secondaryEmail !== "" && secondaryEmail !== null) && (primaryEmail !== "" && primaryEmail !== null)) {
+            primary.val(secondaryEmail);
+            secondary.val(primaryEmail);
+        } else {
+            alert("You must have two email accounts in order to swap");
+        }
+
+    });
+
+    $(document).on("submit", "form#updatePassword", function(e) {
+        e.preventDefault();
+        var form = $(this);
+        var oldPassword = $("#oldPassword").val();
+        var newPassword = $("#oldPassword").val();
+
+        if (oldPassword === newPassword) {
+            alert("Password cannot be the same as your old one.");
+        } else if (newPassword.length < 8) {
+            alert("Password must not be less than 8 characters.")
+        } else {
+            $.ajax({
+                url: form.attr("action"),
+                type: form.attr("method"),
+                dataType: "text",
+                data: form.serialize(),
+                success: function(res) {
+                    console.log(res);
+                },
+                error: function(err) {
+                    console.log(err);
+                }
+            })
+        }
+
+    });
+
 });
--- settings.php
+++ settings.php
@@ -11,50 +11,67 @@
                 <nav class="list-nav">
                     <a href="#">Profile</a>
                     <a href="#">Channels</a>
-                    <a href="#">Privacy</a>
                     <a href="#">Emails</a>
                     <a href="#">Billing</a>
                     <a href="#">Payment</a>
                 </nav>
             </div>
             <div class="settings-body">
-
+                <h2 class="heading">Profile</h2>
+                <form method="post">
+                    <div class="form-group">
+                        <label>Name</label>
+                        <input type="text" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Bio</label>
+                        <textarea class="form-control" rows='5' ></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label>Privacy</label>
+                        <select class="form-control">
+                            <option value="public">Public</option>
+                            <option value="private">Private</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-blue" />
+                        <input type="reset" class="btn btn-default" value="Cancel" />
+                    </div>
+                </form>
+                <h2 class="heading">Change Password</h2>
+                <form id="updatePassword" method="post">
+                    <div class="form-group">
+                        <label>Old password</label>
+                        <input id="oldPassword" type="password" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>New Password</label>
+                        <input id="newPassword" type="password" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-blue" />
+                        <input type="reset" class="btn btn-default" value="Cancel" />
+                    </div>
+                </form>
+                <h2 class="heading">Emails</h2>
+                <form method="post">
+                    <div class="form-group">
+                        <label>Primary Email</label>
+                        <input id="primaryEmail" type="email" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <label>Secondary Email</label>
+                        <input id="secondaryEmail"  type="email" class="form-control" />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-blue" />
+                        <input type="reset" class="btn btn-default" value="Cancel" />
+                        <button id="swapEmails" class="btn btn-default">Swap</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
 </section>
-<!--
-<h2 class="heading">Profile</h2>
-<form method="post">
-    <div class="form-group">
-        <label>Name</label>
-        <input type="text" class="form-control" />
-    </div>
-    <div class="form-group">
-        <label>Bio</label>
-        <textarea class="form-control" rows='5' ></textarea>
-    </div>
-    <div class="form-group">
-        <input type="submit" class="btn btn-blue" />
-        <input type="reset" class="btn btn-default" value="Cancel" />
-    </div>
-</form>
-
-
-<h2 class="heading">Change Password</h2>
-<form method="post">
-    <div class="form-group">
-        <label>Old password</label>
-        <input type="password" class="form-control" />
-    </div>
-    <div class="form-group">
-        <label>New Password</label>
-        <input type="password" class="form-control" />
-    </div>
-    <div class="form-group">
-        <input type="submit" class="btn btn-blue" />
-        <input type="reset" class="btn btn-default" value="Cancel" />
-    </div>
-</form>
- -->
 <?php require "footer.php"; ?>
