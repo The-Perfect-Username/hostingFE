5b4f99144308aaa3e19c68d0177fcd9e9539e228
Lachlan Enderlin <lendo1995@gmail.com>
2017-04-04 23:46:40 +1000

    Designed the basic info for uploads page

--- basic.php
+++ basic.php
@@ -3,9 +3,64 @@
 <script src='./public/js/upload.js' ></script>
 <section class="container">
     <div id="uploadBasicContainer" class="standard-border">
-        <form>
-
-        </form>
+        <h2 class="standard-header">Fill out your basic info</h2>
+        <div id="uploadInfoFormWrapper">
+            <form>
+                <div class="form-group">
+                    <label>Title</label>
+                    <input class="form-control" type="text" maxlength="100" />
+                </div>
+                <div class="form-group">
+                    <label>Add a description</label>
+                    <textarea class="form-control" rows="5"></textarea>
+                </div>
+                <div class="form-group">
+                    <label>Minimum of 5 tags</label>
+                    <input class="form-control" type="text" maxlength="20" />
+                </div>
+                <div class="form-group">
+                    <div class="inline-block">
+                        <label>Privacy</label>
+                        <select class="form-control">
+                            <option value="public">Public</option>
+                            <option value="private">Private</option>
+                            <option value="unlisted">Unlisted</option>
+                        </select>
+                    </div>
+                    &nbsp;
+                    &nbsp;
+                    <div class="inline-block">
+                        <label>Enable Comments</label>
+                        <select class="form-control">
+                            <option value="enabled">Enable</option>
+                            <option value="disabled">Disable</option>
+                        </select>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div id="uploadThumbnailWrapper">
+            <h4>Choose a thumbnail</h4>
+            <div id="previewImage"></div>
+            <div id="previewImageTable">
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+                <div class="small-thumbnails"></div>
+            </div>
+        </div>
+        <div id="uploadBasicFooter">
+            <button class="btn btn-blue">Submit</button>
+            <button class="btn btn-default">Cancel</button>
+        </div>
     </div>
 </section>
 
