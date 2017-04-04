6673373338a39954aa6e83aa5164f1ce4a17d333
Lachlan Enderlin <lendo1995@gmail.com>
2017-04-04 17:17:35 +1000

    .

--- upload.php
+++ upload.php
@@ -2,21 +2,23 @@
 <link href='./public/css/upload.css' rel="stylesheet" />
 <script src='./public/js/upload.js' ></script>
 <section class="container">
-    <form id="uploadForm" class="standard-border">
-        <div class="form-group form-header centre-text">
-            <div class="logo-background">
-                <h1 class="initial">U<h1>
+    <div id="uploadContainer">
+        <form id="uploadForm" class="standard-border">
+            <div class="form-group form-header centre-text">
+                <div class="logo-background">
+                    <h1 class="initial">U<h1>
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <input type="file" id="videoFileUpload" />
-            <label for="videoFileUpload"><span>Upload a file</span></label>
-        </div>
-        <div id="uploadBtnContainer" class="form-group">
-            <input class='btn btn-blue' type="submit" />
-            <input class='btn btn-default' type="reset" value="Cancel" />
-        </div>
-    </form>
+            <div class="form-group">
+                <input type="file" id="videoFileUpload" />
+                <label for="videoFileUpload"><span>Upload a file</span></label>
+            </div>
+            <div id="uploadBtnContainer" class="form-group">
+                <input class='btn btn-blue' type="submit" />
+            </div>
+        </form>
+        <p>Cost of storage is billed monthly. <a href="#">See more on plans</a></p>
+    </div>
 </section>
 
 <?php require "footer.php"; ?>
