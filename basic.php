8cfa6ca2fa0eee11acf82d047303ef59d6eeee75
Lachlan Enderlin <lendo1995@gmail.com>
2017-04-04 17:37:33 +1000

    .

---
+++ basic.php
@@ -0,0 +1,12 @@
+<?php require "header.php"; ?>
+<link href='./public/css/upload.css' rel="stylesheet" />
+<script src='./public/js/upload.js' ></script>
+<section class="container">
+    <div id="uploadBasicContainer" class="standard-border">
+        <form>
+
+        </form>
+    </div>
+</section>
+
+<?php require "footer.php"; ?>
