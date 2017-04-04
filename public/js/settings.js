0e8b7be3e71e3477de069223575691aef555cc0e
Lachlan Enderlin <lendo1995@gmail.com>
2017-04-04 15:38:59 +1000

    Worked on settings. Need to import from other repo

---
+++ public/js/settings.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    $(".list-nav a").on("click", function (e) {
+        e.preventDefault();
+    });
+
+});
---
+++ settings.php
@@ -0,0 +1,60 @@
+<?php require "header.php"; ?>
+<script src='./public/js/settings.js' ></script>
+<link href='./public/css/settings.css' rel="stylesheet" />
+<section class="container">
+    <div class="canvas standard-border">
+        <div class="canvas-header">
+            <h3>Settings</h3>
+        </div>
+        <div class="canvas-body">
+            <div class="settings-menu">
+                <nav class="list-nav">
+                    <a href="#">Profile</a>
+                    <a href="#">Channels</a>
+                    <a href="#">Privacy</a>
+                    <a href="#">Emails</a>
+                    <a href="#">Billing</a>
+                    <a href="#">Payment</a>
+                </nav>
+            </div>
+            <div class="settings-body">
+
+            </div>
+        </div>
+    </div>
+</section>
+<!--
+<h2 class="heading">Profile</h2>
+<form method="post">
+    <div class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control" />
+    </div>
+    <div class="form-group">
+        <label>Bio</label>
+        <textarea class="form-control" rows='5' ></textarea>
+    </div>
+    <div class="form-group">
+        <input type="submit" class="btn btn-blue" />
+        <input type="reset" class="btn btn-default" value="Cancel" />
+    </div>
+</form>
+
+
+<h2 class="heading">Change Password</h2>
+<form method="post">
+    <div class="form-group">
+        <label>Old password</label>
+        <input type="password" class="form-control" />
+    </div>
+    <div class="form-group">
+        <label>New Password</label>
+        <input type="password" class="form-control" />
+    </div>
+    <div class="form-group">
+        <input type="submit" class="btn btn-blue" />
+        <input type="reset" class="btn btn-default" value="Cancel" />
+    </div>
+</form>
+ -->
+<?php require "footer.php"; ?>
